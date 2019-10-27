ca33cbf3cf6adc916d8b29943894d0063ff16c57
Omer Faruk Hasirci <ofhasirci@gmail.com>
2019-10-27 16:16:46 +0300

    Video 136, get and print actor name and position to report

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -19,8 +19,11 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	AActor *Owner = GetOwner();
+	FString ActorName = Owner->GetName();
+	FString ActorLocation = Owner->GetTransform().GetLocation().ToString();
 	
+	UE_LOG(LogTemp, Warning, TEXT("%s location -> %s"), *ActorName, *ActorLocation);
 }
 
 
